c781207846e978b00cac55d7237d55febbf95083
Noel Brown <noelbrowndboss@gmail.com>
2023-06-09 17:51:04 -0700

    incomplete

--- console.py
+++ console.py
@@ -29,13 +29,30 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """Creates a new instance of a class"""
         command = line.split(" ")
-        if commands[0] = "":
+        if command[0] == "":
             print("** class name missing **")
         elif command[0] not in self.__valid_classes:
             print("** class doesn't exist **")
         else:
-            if command[0] = "BaseModel":
+            if command[0] == "BaseModel":
                 x = BaseModel()
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        command = line.split(" ")
+        if command[0] == "":
+            print("** class name missing **")
+        elif command[0] not in self.__valid_classes:
+            print("** class doesn't exist **")
+        
+        // still testing//
+        
+        elif command[1] == 1:
+            print("** instance id missing **")
+        elif command[1] not in self.__valid_classes.id:
+            print("** no instance found **")
+
 
 
 if __name__ == '__main__':
